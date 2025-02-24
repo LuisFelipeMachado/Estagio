ae24ceb11ceb7442700c90966373331082f8803e
Luis Felipe <luisfdsmachadojob@gmail.com>
2025-02-24 10:03:18 -0300

    Validando CPF e Corrigindo alguns erros

--- Formulario/form.php
+++ Formulario/form.php
@@ -1,13 +1,36 @@
 <?php
-    $email = emailuser;
-    $CPF = idcpf;
    
+  $email = "luis.machado@gmail.com";
+
+
   function Validaremail ($email) {
 
 if (filter_var($email,FILTER_VALIDATE_EMAIL)){
-    return true;
+   return true;
 }
- else {
-    return false;
+else {
+  return false;
 }
+  
+if (Validaremail ($email)){
+  echo "Email validado com sucesso";
+}
+ }
+
+ function Validacpf($cpf) {
+  $cpf = $_POST['cpf'];
+  $cpf= preg_replace('/[^0-9]/', '', $cpf);
+  $D1 = 0;
+  $D2= 0;
+
+  for ($i=0, $x= 10; $i <=8 ; $i++, $x--) { 
+    $D1 += $cpf[$i] *  $x;
   }
+  for ($i=0, $x= 11; $i <9 ; $i++, $x--) {
+    $D2 += $cpf[$i] * $x;
+
+    if (str_repeat($i, 11) == $cpf) {
+      echo "Não Repita!!";
+    }
+  }
+ }
